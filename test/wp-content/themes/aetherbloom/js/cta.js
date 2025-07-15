94d57472d31e90b413da8f7c243d6fdf76617cbb
jamesdanielwhitford <james.whitford0@gmail.com>
2025-07-15 14:46:20 +0200

    fix cookie issue

--- test/wp-content/themes/aetherbloom/js/cta.js
+++ test/wp-content/themes/aetherbloom/js/cta.js
@@ -659,7 +659,7 @@
         const value = `; ${document.cookie}`;
         const parts = value.split(`; ${name}=`);
         if (parts.length === 2) return parts.pop().split(';').shift();
-        return '';
+        return null;
     }
     
     // Validate form data
