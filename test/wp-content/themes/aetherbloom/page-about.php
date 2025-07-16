00c198caa92c68d608e49896ea890bd016abfb3a
jamesdanielwhitford <james.whitford0@gmail.com>
2025-07-16 08:47:35 +0200

    udate svg

--- test/wp-content/themes/aetherbloom/page-about.php
+++ test/wp-content/themes/aetherbloom/page-about.php
@@ -57,8 +57,10 @@ get_header(); ?>
                         <div class="foundation-cards">
                             <div class="foundation-card active" data-target="mission">
                                 <div class="card-icon">
-                                    <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 2L2 7V12C2 17.55 5.84 22.54 9.35 23.85C10.15 24.05 11.15 24.05 12 24C12.85 24.05 13.85 24.05 14.65 23.85C18.16 22.54 22 17.55 22 12V7L12 2Z" stroke="currentColor" stroke-width="2"/>
+                                    <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <circle cx="12" cy="12" r="10"></circle>
+                                        <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                                        <path d="M2 12h20"></path>
                                     </svg>
                                 </div>
                                 <h3><?php esc_html_e('Our Mission', 'aetherbloom'); ?></h3>
