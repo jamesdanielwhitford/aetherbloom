698e981d1a7b14fee045f103796600f5fa539b56
jamesdanielwhitford <james.whitford0@gmail.com>
2025-07-16 08:48:23 +0200

    svg update

--- test/wp-content/themes/aetherbloom/page-about.php
+++ test/wp-content/themes/aetherbloom/page-about.php
@@ -78,9 +78,8 @@ get_header(); ?>
                             
                             <div class="foundation-card" data-target="values">
                                 <div class="card-icon">
-                                    <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M19 14C19 18.4 15.4 22 11 22C6.6 22 3 18.4 3 14C3 10.6 6.6 7 11 7C15.4 7 19 10.6 19 14Z" stroke="currentColor" stroke-width="2"/>
-                                        <path d="M9 12L11 14L15 10" stroke="currentColor" stroke-width="2"/>
+                                    <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
                                     </svg>
                                 </div>
                                 <h3><?php esc_html_e('Our Values', 'aetherbloom'); ?></h3>
