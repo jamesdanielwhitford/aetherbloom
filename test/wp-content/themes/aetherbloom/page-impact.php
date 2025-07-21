5cbc832d4a2e883e2e881864fd0a6ac1f117ca4b
jamesdanielwhitford <james.whitford0@gmail.com>
2025-07-21 12:05:24 +0200

    Update icon

--- test/wp-content/themes/aetherbloom/page-impact.php
+++ test/wp-content/themes/aetherbloom/page-impact.php
@@ -149,10 +149,8 @@ get_header(); ?>
                         <div class="transformation-grid">
                             <div class="transformation-card">
                                 <div class="transformation-icon">
-                                    <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="12" cy="12" r="10" stroke="#39564F" stroke-width="2"/>
-                                        <path d="M9.09 9C9.3251 8.33167 9.78915 7.76811 10.4 7.40913C11.0108 7.05016 11.7289 6.91894 12.4272 7.03871C13.1255 7.15849 13.7588 7.52152 14.2151 8.06353C14.6713 8.60553 14.9211 9.29152 14.92 10C14.92 12 11.92 13 11.92 13" stroke="#39564F" stroke-width="2"/>
-                                        <path d="M12 17H12.01" stroke="#39564F" stroke-width="2"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-currency-pound" viewBox="0 0 16 16">
+                                        <path d="M4 8.585h1.969c.115.465.186.939.186 1.43 0 1.385-.736 2.496-2.075 2.771V14H12v-1.24H6.492v-.129c.825-.525 1.135-1.446 1.135-2.694 0-.465-.07-.913-.168-1.352h3.29v-.972H7.22c-.186-.723-.372-1.455-.372-2.247 0-1.274 1.047-2.066 2.58-2.066a5.3 5.3 0 0 1 2.103.465V2.456A5.6 5.6 0 0 0 9.348 2C6.865 2 5.322 3.291 5.322 5.366c0 .775.195 1.515.399 2.247H4z"/>
                                     </svg>
                                 </div>
                                 <h4><?php esc_html_e('Living wages that transform households', 'aetherbloom'); ?></h4>
