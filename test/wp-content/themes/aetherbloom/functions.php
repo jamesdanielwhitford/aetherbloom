47812a1a5c226b3c124f4e61bd33e44adeee649d
jamesdanielwhitford <james.whitford0@gmail.com>
2025-07-23 08:40:18 +0200

    Optimize

--- test/wp-content/themes/aetherbloom/functions.php
+++ test/wp-content/themes/aetherbloom/functions.php
@@ -860,3 +860,10 @@ function aetherbloom_add_canonical() {
     }
 }
 add_action('wp_head', 'aetherbloom_add_canonical');
+
+function aetherbloom_remove_block_library_css() {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('global-styles');
+}
+add_action('wp_enqueue_scripts', 'aetherbloom_remove_block_library_css', 100);
